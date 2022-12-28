4e04f78ea5725ad98bace6b17e444d3c582c3569
aux2022 <aux_ti@bioflex.mx>
2022-12-28 11:48:25 -0600

    falta suma

---
+++ Controllers/GetSuperAreaController.cs
@@ -0,0 +1,31 @@
+﻿using back_salidaActivos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace back_salidaActivos.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "GET,POST,PUT,DELETE,OPTIONS")]
+    public class GetSuperAreaController : ApiController
+    {
+        // GET: api/GetSuperArea
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/GetSuperArea/5
+        public IEnumerable<solicitud> Get(string id)
+        {
+            GestorSolicitud gSolicitud = new GestorSolicitud();
+            return gSolicitud.GetSolicitudsByIdSA(id);
+
+        }
+
+       
+    }
+}
---
+++ Controllers/SolicitudesCerradasPorAreaController.cs
@@ -0,0 +1,42 @@
+﻿using back_salidaActivos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace back_salidaActivos.Controllers
+{
+    public class SolicitudesCerradasPorAreaController : ApiController
+    {
+        // GET: api/SolicitudesCerradasPorArea
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/SolicitudesCerradasPorArea/5
+        public IEnumerable<solicitud> Get(string id)
+        {
+            GestorSolicitud gSolicitud = new GestorSolicitud();
+            return gSolicitud.GetSolicitudsCerradasPorArea(id);
+
+        }
+
+        // POST: api/SolicitudesCerradasPorArea
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/SolicitudesCerradasPorArea/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SolicitudesCerradasPorArea/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
